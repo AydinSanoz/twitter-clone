fe38cf89b8cbca1c37a90fb733511ab78815440d
E2029-Aydin <aydinsanoz2@gmail.com>
2021-02-06 23:39:28 +0300

    scrollbar visibility removed from feed section

--- src/Feed.js
+++ src/Feed.js
@@ -1,14 +1,42 @@
 import React from 'react';
-import './Feed.css'
+import Post from './components/Post';
+import TweetBox from './components/TweetBox';
+import './Feed.css';
 
 function Feed() {
 	return (
-		<div>
+		<div className="feed">
 			{/* Heaer */}
-			<div className="feed">
+			<div className="feed-header">
 				<h2>Home</h2>
 			</div>
 			{/* TweetBox */}
+            <TweetBox />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            
 			{/* Post */}
 			{/* Post */}
 			{/* Post */}
