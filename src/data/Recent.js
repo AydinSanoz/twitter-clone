8622b409fb25c6a0bb30a3d54a9b8bc06aa3dcf3
E2029-Aydin <aydinsanoz2@gmail.com>
2021-02-08 12:18:14 +0300

    sample data created and added to db

---
+++ src/data/Recent.js
@@ -0,0 +1,20 @@
+import React,{useState} from 'react';
+import Button from '../components/Button';
+import { data } from './index';
+import db from '../firebase';
+
+const RecentTweets = (props) => {
+	const feedRecentTweets = (e) => {
+		e.preventDefault();
+		data.map((tweet) => db.collection('posts').add({tweet}));
+		;
+	};
+	return (
+		<div>
+			<h3>Recent Tweets</h3>
+			<Button onClick={feedRecentTweets}>Recent Tweets</Button>
+		</div>
+	);
+};
+
+export default RecentTweets;
