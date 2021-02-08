62937accc2bcef137f5f198153f8bd5b7e2f566e
E2029-Aydin <aydinsanoz2@gmail.com>
2021-02-08 21:40:48 +0300

    tweets fetched from data and listed at Explorer Page

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Feed from '../components/Feed';
 import TweetBox from '../components/TweetBox';
+import SearchBox from '../components/SearchBox';
+import {data} from '../data/index';
+import Post from '../components/Post';
 
 function Explore() {
+	useEffect(() => {
+		
+	}, []);
+
 	return (
 		<div className="feed">
 			<div className="feed-header">
 				<h2>Explore</h2>
+				<SearchBox />
 			</div>
-			<TweetBox />
+			{data.map(tweet=><Post
+					avatar={tweet.avatar_img}
+					datetime={tweet.datetime}
+					id={tweet.id}
+					name={tweet.name}
+					slug={tweet.slug}
+					text={tweet.text}
+					// like= {tweet.textInfo.like}
+					// reply= {tweet.textInfo.reply}
+					// retweet= {tweet.textInfo.retweet}
+					tweet_img={tweet.tweet_img}
+				/>)}
+			
 			<Feed />
 		</div>
 	);
