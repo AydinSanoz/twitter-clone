a91f57d0b17387781748dd33b2f4090c2dcd8b67
E2029-Aydin <aydinsanoz2@gmail.com>
2021-02-09 19:35:13 +0300

    routing bug corrected

--- src/Router/AppRouter.js
+++ src/Router/AppRouter.js
@@ -2,19 +2,12 @@ import React, { useContext } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Welcome from '../pages/Welcome';
 import Main from './MainRouter';
-import {FirebaseAuthContext} from '../context/AuthContext';
+import { FirebaseAuthContext } from '../context/AuthContext';
 
 function AppRouter() {
-	const {currentUser}  = useContext(FirebaseAuthContext);
+	const { currentUser } = useContext(FirebaseAuthContext);
 	console.log('current', currentUser);
 
-	return (
-		<Router>
-			<Switch>
-				<Route exact path="/" component={!currentUser ? Welcome : Main} />
-			</Switch>
-		</Router>
-	);
+	return currentUser ? <Main /> : <Welcome />;
 }
-
 export default AppRouter;
--- src/Router/MainRouter.js
+++ src/Router/MainRouter.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useContext} from 'react';
 import Sidebar from '../pages/sections/Sidebar';
 import Home from '../pages/Home';
 import Explore from '../pages/Explore';
 import Error from '../pages/Error';
 import Widgets from '../pages/sections/Widgets';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Signin from '../pages/Signin';
-import Signup from '../pages/Signup';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+} from 'react-router-dom';
+import { FirebaseAuthContext } from '../context/AuthContext';
+import Welcome from '../pages/Welcome';
 
 function Main() {
+	const { currentUser } = useContext(FirebaseAuthContext);
+
 	return (
 		<Router>
 			<Sidebar />
 			<Switch>
-				<Route path="/Home" component={Home} exact />
-				<Route path="/Explore" component={Explore} exact />
-				<Route path="" component={Error} />
+				<Route exact path="/Home" component={currentUser ? Home : Welcome} />
+				<Route
+					exact
+					path="/Explore"
+					component={currentUser ? Explore : Welcome}
+				/>
+				<Route path="/Notifications" component={Error} />
+				<Route path="/Messages" component={Error} />
+				<Route path="/Bookmarks" component={Error} />
+				<Route path="/Lists" component={Error} />
+				<Route path="/Profile" component={Error} />
+				<Route path="/More" component={Error} />
+				<Route path="" component={Home} />
 			</Switch>
 			<Widgets />
 		</Router>
