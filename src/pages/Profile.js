35dc2974bd5e5c4613b3534b80bbf552dab62e50
E2029-Aydin <aydinsanoz2@gmail.com>
2021-02-10 15:36:44 +0300

    profile metada added from currentsUser metada

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,42 +1,28 @@
 import React, { useContext } from 'react';
 import { FirebaseAuthContext } from '../context/AuthContext';
-import Feed from '../components/Feed';
+import ProfileBox from '../components/ProfileBoxComp';
 import './Profile.css';
-import Avatar from '../components/Avatar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Feed from '../components/Feed';
 
 function Profile() {
 	const { currentUser } = useContext(FirebaseAuthContext);
 	const userName = currentUser.email.split('@');
 
-	console.log('current', currentUser.photoURL);
-
+	console.log('current', currentUser);
 
 	return (
 		<div className="feed">
 			<div className="feed-header">
 				<h2>Profile</h2>
 			</div>
-			<div className="profile-box">
-				<div className="profile-img-box">
-					<div className="profile-img">
-						<img src="#" alt="" />
-						<div className="avatar">
-							<Avatar
-								avatar={currentUser.photoURL}
-								cn="avatar-img-large"
-							/>
-						</div>
-					</div>
-				</div>
-				<div className="profile-data">
-					<h2 className="profile-name">
-						{userName[0].charAt(0).toUpperCase() +
-							userName[0].slice(1).toLowerCase()}
-					</h2>
-					<p className="profile-displayName">{currentUser.displayName}</p>
-				</div>
-				<div className="profile-tweets">{/* <Feed /> */}</div>
-			</div>
+			<ProfileBox />
+			{/* <Router>
+				<Switch>
+					<Route path="/Profile" component={Error} />
+					<Route path="/Profile/Tweets" component={Feed} />
+				</Switch>
+			</Router> */}
 		</div>
 	);
 }
